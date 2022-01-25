df4bbaa0d1fe81669dccc6e727a9c6a0e3275e25
xiaojie xu <xiaojie.xu@mail.utoronto.ca>
2022-01-25 15:15:33 -0500

    added two sh files

---
+++ GraphCodeBERT/clonedetection/fine-tune.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#SBATCH --time=40:00:00
+#SBATCH --account=def-six
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mem=16G
+#SBATCH --job-name=GCBfinetune
+
+source ../../pyvenv/venv/bin/activate
+
+dataset_dir=dataset
+
+mkdir saved_models
+python run.py \
+    --output_dir=saved_models \
+    --config_name=microsoft/graphcodebert-base \
+    --model_name_or_path=microsoft/graphcodebert-base \
+    --tokenizer_name=microsoft/graphcodebert-base \
+    --do_train \
+    --train_data_file=${dataset_dir}/train.txt \
+    --eval_data_file=${dataset_dir}/valid.txt \
+    --test_data_file=${dataset_dir}/test.txt \
+    --epoch 1 \
+    --code_length 512 \
+    --data_flow_length 128 \
+    --train_batch_size 16 \
+    --eval_batch_size 32 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --evaluate_during_training \
+    --seed 123456 2>&1| tee saved_models/train.log
